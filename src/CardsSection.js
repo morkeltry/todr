1187f7135a3f02454ab086ff93d9783273540911
monkeybrie <morkeltry@mailfence.com>
2019-06-05 20:28:14 +0100

    empty app with BlankComponent and blank CardsSection; renders.

---
+++ src/CardsSection.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+// import OtherComponent from './OtherComponent';
+// import './CardsSection.css';
+
+class CardsSection extends Component {
+  constructor () {
+    super ();
+    this.state = {
+    }
+  };
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  };
+}
+
+export default CardsSection;
